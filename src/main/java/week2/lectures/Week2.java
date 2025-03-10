9ae5dc3fc06aa044fdae02d9d31e0f32da2ad8c7
ibecir <becir@bioptimizers.com>
2025-03-10 09:25:21 +0100

    Add two methods important for quiz

--- src/main/java/week2/lectures/Week2.java
+++ src/main/java/week2/lectures/Week2.java
@@ -25,25 +25,71 @@ public class Week2 {
     }
 
     public static void main(String[] args) {
-        printHalfPyramidShapeOfNRows(5);
-
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Please enter first number: ");
-        int firstNumber = reader.nextInt();
-
-        System.out.println("Please enter second number: ");
-        int end = reader.nextInt();
-
-        Week2 objInstance = new Week2();
-
-        objInstance.printNumbersInRange(firstNumber, end);
+        System.out.println(sumOfPrimeDigits(12578));
+//        printHalfPyramidShapeOfNRows(5);
+//
+//        Scanner reader = new Scanner(System.in);
+//
+//        System.out.println("Please enter first number: ");
+//        int firstNumber = reader.nextInt();
+//
+//        System.out.println("Please enter second number: ");
+//        int end = reader.nextInt();
+//
+//        Week2 objInstance = new Week2();
+//
+//        objInstance.printNumbersInRange(firstNumber, end);
     }
 
-    public void printNumbersInRange(int start, int end) {
+    public static void printNumbersInRange(int start, int end) {
         for (int i = start; start <= end; start++) {
             System.out.println(i);
             i++;
         }
+    }
+
+    public static int sumOfPrimeDigits(int n) {
+        int sum = 0;
+        while(n > 0) {
+            int tmp = n % 10;
+            int i_sum = 0;
+            for (int j = 2; j * j <= tmp; j++) {
+                if(tmp % j == 0){
+                    i_sum += j;
+                }
+            }
+            n = n / 10;
+            if(tmp == i_sum){
+                sum += tmp;
+            }
+        }
+        return sum;
+    }
+
+    public static int sumOfPrimeDigitsBetter(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            if (isPrime(digit)) {
+                sum += digit;
+            }
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    // Helper method to check if a number is prime using loops
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i = 2; i * i <= num; i++) { // Only check up to √num
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }

05b257e91d54f0dccebf66a0a76d1336b4bfbea5
ibecir <becir@bioptimizers.com>
2025-04-29 15:23:50 +0100

    Week 10 materials

--- src/main/java/week10/lectures/optionals/OptionalsExample.java
+++ src/main/java/week10/lectures/optionals/OptionalsExample.java
@@ -16,16 +16,21 @@ class OptionalsExample {
 
         Optional<Human> human = Optional.of(new Human("Becir", 29));
         Human humanNullable = Optional
-                .ofNullable(new Human("Becir", 29))
+                .of(new Human("Becir", 29))
                 .orElse(null);
+        System.out.printf("Human is: %s%n", humanNullable);
 
-        String name = (String) Optional
-                .ofNullable(null)
+        String name = Optional
+                .ofNullable("Top")
                 .orElseGet(() -> "Becir");
+
+        System.out.printf(name + " is %s\n", human);
 
         Optional<Human> humanOptional = humans
                 .stream()
                 .filter(humanFilter -> humanFilter.name().equals("Becir"))
                 .findFirst();
+
+        System.out.printf(humanOptional.get().name());
     }
 }

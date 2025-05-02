14d8cf21a4ec003a970ce7d57197685f16bc295d
ibecir <becir@bioptimizers.com>
2025-05-02 19:49:04 +0100

    Week 10 materials

---
+++ src/main/java/week10/lectures/hashmaps/StudentMap.java
@@ -0,0 +1,97 @@
+package week10.lectures.hashmaps;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+class StudentMap {
+    public static void main(String[] args) {
+        // Similar as the List<T >, it is using the templates (generics)
+        // The diamond operator has to be used even as it is empty
+        // because it is the legacy of older Java versions
+        // (the types are inferred from the left-hand sand)
+        HashMap<Integer, Student> students = new HashMap<>();
+
+        students.put(123, new Student(123, 15, "Becir"));
+        students.put(456, new Student(456, 13, "Mirza"));
+
+        // It is not necessarily maintaining an order
+        System.out.println(students);
+
+        // Get the value by the key
+        System.out.println(students.get(123));
+
+        System.out.println(students.containsKey(123));
+        System.out.println(students.containsKey(234));
+
+        System.out.println(students.containsValue(new Student(456, 13, "Mirza")));
+        System.out.println(students.containsValue(new Student(888, 13, "Mirza")));
+
+        // Key is what is important, and if you redeclare it is going to override
+        // the old value under that certain key
+        students.put(456, new Student(666, 99, "Amila"));
+        System.out.println(students);
+
+        // replace() method will only replace the value under the key if the
+        // key exists and neglect it otherwise, while the put will replace it
+        // or add it if it does not exist
+        students.replace(456, new Student(666, 99, "Becir"));
+
+        // This value will not be added
+        students.replace(999, new Student(666, 99, "Becir"));
+        System.out.println(students);
+
+        // The putIfAbsent is only used to insert into the map if the value
+        // does not exist, it will not override the value if it exists in the map already
+        students.putIfAbsent(9898, new Student(666, 99, "Becir"));
+        System.out.println(students);
+
+        students.remove(9898);
+        System.out.println(students);
+    }
+}
+
+class Student {
+    private final int id;
+    private final String name;
+    private final int age;
+
+    public Student(int id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student class -> " + this.id + " " + this.name + " " + this.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Student) {
+            Student student = (Student) obj;
+            return this.id == student.id && this.name.equals(student.name) && this.age == student.age;
+        }
+        return false;
+    }
+}
+
+record StudentR(int id, String name, int age) {
+}
--- src/main/java/week10/lectures/records/Person.java
+++ src/main/java/week10/lectures/records/Person.java
@@ -2,8 +2,17 @@ package week10.lectures.records;
 
 import java.util.Objects;
 
+// Record cannot inherit any class
+// Records are also, by default, final, meaning that they cannot be extended
+// by any other class
+// You are allowed to implement interfaces in the records
+//  are only used to carry the data, and they are immutable (cannot be changed)
 record Person(String name, int age, int height) {
     public static String UNKNOWN_ADDRESS = "Unknown";
+
+    // Instance fields are not allowed in the record body, only in the signature
+    // of the records in the brackets
+    // private String address;
 
     public Person(String name) {
         this(name, 0, 0);
@@ -13,8 +22,22 @@ record Person(String name, int age, int height) {
         this(name, age, 0);
     }
 
+    // Referred to as a compact constructor, unique only to records
+    // Same like the canonical constructor, you do not have to specify
+    // parameters that it accepts as by default it accepts all instance
+    // fields and sets their values. Used only to impose extra restrictions
+    // on the fields like we did it below
+    public Person {
+        if (age > 100)
+            throw new IllegalArgumentException("Age must be less than 100");
+    }
+
     public static Person unnamed(String name) {
         return new Person(name);
+    }
+
+    public static void printUppercase(String name) {
+        System.out.println(name.toUpperCase());
     }
 
     public int a() {
@@ -23,10 +46,15 @@ record Person(String name, int age, int height) {
 }
 
 class PersonC {
-    private String name;
-    private int age;
-    private int height;
+    private final String name;
+    private final int age;
+    private final int height;
 
+    // By default, records have an all-arguments constructor which is called
+    // canonical constructor. This is unlike the normal Java class whose
+    // default constructor is no argument constructor. The canonical constructor
+    // is responsible for setting the values for the properties during the
+    // instantiation of the object
     public PersonC(String name, int age, int height) {
         this.name = name;
         this.age = age;
@@ -37,24 +65,12 @@ class PersonC {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public int getHeight() {
         return height;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
     }
 
     @Override

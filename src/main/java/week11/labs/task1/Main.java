36d46fc273985d47ecde0c6268e404aca5648654
Mirza Krupic <mirzakrupic@Mirzas-MacBook-Pro.local>
2025-05-12 13:10:14 +0200

    changed order of tasks

--- src/main/java/week11/labs/task1/Main.java
+++ src/main/java/week11/labs/task1/Main.java
@@ -1,46 +1,37 @@
-package week11.labs.task1;
+package week11.labs.task2;
 
-import java.lang.reflect.*;
+import java.lang.reflect.Method;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws Exception {
-        Dog dog = new Dog("Rex", 5);
+        ProjectTasks tasks = new ProjectTasks();
+        Method[] methods = tasks.getClass().getDeclaredMethods();
 
-        Class<?> clazz = dog.getClass();
-        System.out.println("Class: " + clazz.getName());
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(TrackTask.class)) {
+                TrackTask annotation = method.getAnnotation(TrackTask.class);
+                String assignedTo = annotation.assignedTo();
+                int priority = annotation.priority();
 
-        System.out.println("\nFields:");
-        for (Field field : clazz.getDeclaredFields()) {
-            field.setAccessible(true);
-            System.out.println(field.getType().getSimpleName() + " " + field.getName() + " = " + field.get(dog));
-        }
+                String priorityStr = switch (priority) {
+                    case 3 -> "HIGH";
+                    case 2 -> "MEDIUM";
+                    default -> "LOW";
+                };
 
-        System.out.println("\nMethods:");
-        for (Method method : clazz.getDeclaredMethods()) {
-            System.out.print(method.getReturnType().getSimpleName() + " " + method.getName() + "(");
-            Class<?>[] params = method.getParameterTypes();
-            for (int i = 0; i < params.length; i++) {
-                System.out.print(params[i].getSimpleName());
-                if (i < params.length - 1) System.out.print(", ");
+                System.out.println("Method: " + method.getName());
+                System.out.println("Assigned To: " + assignedTo);
+                System.out.println("Priority: " + priorityStr);
+                System.out.println();
+
+                if (priority == 3 &&
+                        !(method.getName().toLowerCase().contains("critical") ||
+                                method.getName().toLowerCase().contains("important"))) {
+                    System.out.println("Warning: Method " + method.getName() +
+                            " has high priority but is not labeled as 'Critical' or 'Important'");
+                    System.out.println();
+                }
             }
-            System.out.println(")");
         }
-
-        Field nameField = clazz.getDeclaredField("name");
-        nameField.setAccessible(true);
-        nameField.set(dog, "Max");
-        System.out.println("\nModified name: " + nameField.get(dog));
-
-        Field ageField = clazz.getDeclaredField("age");
-        ageField.setAccessible(true);
-        ageField.set(dog, 8);
-        System.out.println("Modified age: " + ageField.get(dog));
-
-        clazz.getDeclaredMethod("bark").invoke(dog);
-        clazz.getDeclaredMethod("say", String.class).invoke(dog, "Let's play!");
-        Method privateMethod = clazz.getDeclaredMethod("whisper");
-        privateMethod.setAccessible(true);
-        privateMethod.invoke(dog);
-        clazz.getDeclaredMethod("info").invoke(null);
     }
 }
